dbfe2b8dc48cdcd0d6cd482d2cb79a4ccc14d04a
Kaan Tunçer <54275187+kaantuncer@users.noreply.github.com>
2021-05-02 12:45:20 +0300

    Delete credentials.js

--- Bot/src/credentials.js
+++
@@ -1,6 +0,0 @@
-module.exports.youtubeAPI = "AIzaSyCUoOnT4uMC5-AikTXgBjrQJxk6oQdOCAQ"
-module.exports.spotifyToken = "BQCo17e4qGTXewocckma4P3QyWikELBjG_gbIueoTK1yAknv4_gBtcpj78bG7MC9ysmYubCEcCG_0ZgXPiXM87Ajg0SD9HyHRYmohCtu3VFXwHqD5NMfPfU6Wcc1Xp4Itdo50mG9dy8NEhicn2WSHkfcSGbdxveD"
-module.exports.botToken = "NjU4NDAwMjE5MjI2MDQ2NTI0.Xf_M_A.IMNHCJSyFZpuC8ffGbYQAg5xwyA"
-module.exports.databaseAPI = "https://discord-bot-database.herokuapp.com/"
-module.exports.spotifyClientId = "c5d5bf23a4cc4c91b903dd13b80360f5";
-module.exports.spotifyClientSecret = "96793297bdaf48c894279bcfbf9dfb39";
